2e3916fb416085db7327d043231e1d973c58764b
Escher WD <escherwd@yahoo.com>
2025-10-15 17:58:23 -0700

    Temporarily disable new project route

--- src/app/new/route.ts
+++ src/app/new/route.ts
@@ -3,21 +3,23 @@ import prisma from "@/backend/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
+  // TODO: This should be server function logic or something
+  return NextResponse.json({ error: "Temporarily disabled" });
 
-    const user = await getUser();
+  const user = await getUser();
 
-    if (!user) {
-        return NextResponse.redirect(new URL('/login', request.url))
-    }
+  if (!user) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
-    // Create a new project
-    const project = await prisma.project.create({
-        data: {
-            name: 'Untitled Trip',
-            userId: user.id,
-            slug: crypto.randomUUID().slice(0, 8),
-        }
-    })
+  // Create a new project
+  const project = await prisma.project.create({
+    data: {
+      name: "Untitled Trip",
+      userId: user.id,
+      slug: crypto.randomUUID().slice(0, 8),
+    },
+  });
 
-    return NextResponse.redirect(new URL(`/t/${project.slug}`, request.url))
+  return NextResponse.redirect(new URL(`/t/${project.slug}`, request.url));
 }
