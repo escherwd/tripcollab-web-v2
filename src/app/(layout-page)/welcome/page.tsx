26093261f43a822b4978b8bbe0854dcc2b95cd7f
Escher WD <escherwd@yahoo.com>
2026-02-15 20:03:47 -0800

    update wording on welcome page

--- src/app/(layout-page)/welcome/page.tsx
+++ src/app/(layout-page)/welcome/page.tsx
@@ -5,44 +5,53 @@ import Link from "next/link";
 export default async function WelcomePage() {
   return (
     <>
-    <style>{'#mobile-overlay { display: none }'}</style>
-    <div className="pt-12 pb-24 px-4">
-      <div className="w-full aspect-[3.6] rounded-lg overflow-hidden mx-auto max-w-3xl">
-        <img
-          src="/images/tripcollab-banner.webp"
-          alt="Banner Image"
-          className="object-fill"
-        />
+      <style>{"#mobile-overlay { display: none }"}</style>
+      <div className="pt-12 pb-24 px-4">
+        <div className="w-full aspect-[3.6] rounded-lg overflow-hidden mx-auto max-w-3xl">
+          <img
+            src="/images/tripcollab-banner.webp"
+            alt="Banner Image"
+            className="object-fill"
+          />
+        </div>
+        <main className="mx-auto max-w-xl">
+          <h1 className="mt-12 font-display text-xl font-medium">
+            Introduction
+          </h1>
+          <p className="mt-3 text-gray-700">
+            <span className="italic">
+              Tripcollab: Developing a Collaborative Spatial and Temporal
+              Platform for Visualizing Complex Travel Itineraries
+            </span>
+            <br />
+            <br />
+            This thesis explores how the integration of interactive map-based
+            spatial visualizations and schedule-based temporal visualizations
+            can improve the user experience of collaborative trip planning
+            through iterative design and user testing. The goal of this project
+            is to develop an online platform for visualizing complex travel
+            itineraries, with a focus on user-friendliness and accessibility.
+          </p>
+          <h1 className="mt-12 font-display text-xl font-medium">
+            Beta Testing
+          </h1>
+          <p className="mt-3 text-gray-700">
+            The platform is currently in a closed beta. Are you a tester?{" "}
+            <Link className="underline" href="/login">
+              Click here
+            </Link>{" "}
+            to log in.
+          </p>
+          <h1 className="mt-12 font-display text-xl font-medium">Feedback</h1>
+          <p className="mt-3 text-gray-700">
+            Please contact{" "}
+            <a href="mailto:wrighesc@oregonstate.edu" className="underline">
+              Escher
+            </a>{" "}
+            (wrighesc@oregonstate.edu) to provide feedback.
+          </p>
+        </main>
       </div>
-      <main className="mx-auto max-w-xl">
-        <h1 className="mt-12 font-display text-xl font-medium">Introduction</h1>
-        <p className="mt-3 text-gray-700">
-          The purpose of this project is to develop an online platform for
-          visualizing complex travel itineraries, combining spacial interfaces
-          through the use of maps and temporal interfaces through the use of
-          timelines and calendars. Through thoughtful experience design,
-          iterative user-testing, and integrated collaboration features, the
-          goal of this project is to create a user-friendly platform that
-          addresses the challenges of group travel planning.
-        </p>
-        <h1 className="mt-12 font-display text-xl font-medium">Beta Testing</h1>
-        <p className="mt-3 text-gray-700">
-          This website is currently in a closed beta. Are you a tester?{" "}
-          <Link className="underline" href="/login">
-            Click here
-          </Link>{" "}
-          to log in.
-        </p>
-        <h1 className="mt-12 font-display text-xl font-medium">Feedback</h1>
-        <p className="mt-3 text-gray-700">
-          Please contact{" "}
-          <a href="mailto:wrighesc@oregonstate.edu" className="underline">
-            Escher
-          </a>{" "}
-          (wrighesc@oregonstate.edu) to provide feedback.
-        </p>
-      </main>
-    </div>
     </>
   );
 }
